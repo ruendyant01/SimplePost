aaf622da4840f7b924635e4c455389640f302a11
ruendyant01 <ruendyant.mayraclle@gmail.com>
2023-03-12 09:20:57 +0700

    fix: validation message and redirecting on create form

--- frontend/src/pages/components/ErrorComponent.jsx
+++ frontend/src/pages/components/ErrorComponent.jsx
@@ -1,22 +1,36 @@
 import { Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
-export default function ErrorComponent({error}) {
+export default function ErrorComponent({error, type}) {
     const [errorMsg, setErrorMsg] = useState();
 
     useEffect(() => {
-      if(error.code === "ERR_NETWORK") {
-        setErrorMsg(error.message)
-      } else {
-        setErrorMsg(error.response.statusText);
+      if(type !== "message") {
+        if(error.code === "ERR_NETWORK") {
+          setErrorMsg(error.message)
+        } else if (type === "message") {
+  
+        } else {
+          setErrorMsg(error.response.statusText);
+        }
       }
     }, [])
 
     return (
+      <>
+        {
+          type === "message" ? 
+          <Alert status='error'>
+            <AlertIcon />
+            <AlertDescription>{error}</AlertDescription>
+          </Alert> : 
         <Alert status='error'>
           <AlertIcon />
           <AlertTitle>Something Wrong</AlertTitle>
           <AlertDescription>{errorMsg}</AlertDescription>
         </Alert>
+        }
+      </>
+        
     )
 }
--- frontend/src/pages/create-form.jsx
+++ frontend/src/pages/create-form.jsx
@@ -1,27 +1,38 @@
 import { makePost } from '@/actions/action';
 import mutation from '@/helpers/mutation';
 import { Button, Container, Flex, Heading, Input, Textarea } from '@chakra-ui/react'
-import { useRouter } from 'next/router';
-import { useState } from 'react';
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import ErrorComponent from './components/ErrorComponent';
 
 
 export default function Create() {
     const mutations = mutation(makePost);
-    const {register, handleSubmit} = useForm();
+
+    const schemas = yup.object({
+        title: yup.string().required(),
+        body: yup.string().min(10).required()
+    }).required();
+
+    const {reset,register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schemas)
+    });
 
     const hanSubmit = (data) => {
         mutations.mutate(data);
+        reset();
     }
 
     return <Container>
         <form onSubmit={handleSubmit(hanSubmit)}>
         <Flex alignItems="center" flexDirection="column">
             <Heading marginY="1rem" as="h1" size="xl" >Create Post</Heading>
-            <Input placeholder='Title' marginY="1rem" {...register("title", {required:true})}/>
-            <Textarea placeholder='Body'marginY="1rem" {...register("body", {required:true, min:10})}/>
+            <Input as="input" {...(errors.title?.message && {isInvalid:true})} placeholder='Title' marginY="1rem" {...register("title")}/>
+            {errors.title?.message && <ErrorComponent type="message" error={errors.title.message} />}
+            <Textarea as="textarea" placeholder='Body'marginY="1rem" {...register("body")}/>
+            {errors.body?.message && <ErrorComponent type="message" error={errors.body.message} />}
             <Button {...(mutations.isLoading ? {isLoading:true, loadingText:"Processing"} : {})} colorScheme='linkedin' mt="2rem" type='submit'>Add</Button>
-            
         </Flex>
         </form>
     </Container>
